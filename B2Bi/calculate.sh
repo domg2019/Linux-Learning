91ea25a616cb3e45e661703f00d9c55da6bb8457
domg2019 <49177986+domg2019@users.noreply.github.com>
2019-11-11 17:24:37 +0800

    new update
    1.delete the 0 byte file after handling tickets;
    2. add the scripts to handle serial massfilter files

--- B2Bi/calculate.sh
+++ B2Bi/calculate.sh
@@ -24,7 +24,32 @@ do if [ `grep -o Parallel $FILE|uniq|wc -l|bc` -eq 1 ]
                         echo -e -n "Number of files: $c_check"
                         echo -e "+++++++++++++++++++++++++++"
                 done
+		rm -rf $FILE
 	elif [ `grep -o SerialLarge $FILE|uniq|wc -l|bc` -gt 0 ]
-		then echo -e "There are Serical Massfilters !!!!!!!" 
+		then c_line=`cat $FILE | grep -n -E 'CET$' | awk -F: '{print $1}' | wc -l`
+                 for ((c_check=1;c_check<=$c_line;c_check++))
+                 do 	#create singal txt only contains one massfilter result
+						f_PID=`echo $FILE|awk -F. '{print $1}'`
+                        touch /app/sword/schenker/support/luis/$f_PID.$c_check.txt
+                        #Get the last line for one process	
+						A=`grep -n -E '^Deleted' $FILE | awk -F: '{print $1}' |  sed -n 1p`
+						X=`echo $A+1 | bc`
+						#Caculate the last 30 lines:Y as it's serial
+						Y=`echo $X-30 | bc`		
+                        #Get the remafi line:
+                        R=`grep -n -E '^>>>remafi' $FILE | awk -F: '{print $1}' |  sed -n 1p`
+                        S=`echo $R+5 | bc`
+                        if [ "$X" -gt 39 ];then
+                                sed -n 1,"$S"p $FILE  >> /app/sword/schenker/support/luis/$f_PID.$c_check.txt
+                                echo "......" >> /app/sword/schenker/support/luis/$f_PID.$c_check.txt
+                                sed -n $Y,"$X"p $FILE  >> /app/sword/schenker/support/luis/$f_PID.$c_check.txt
+                        else sed -n 1,"$X"p $FILE >> /app/sword/schenker/support/luis/$f_PID.$c_check.txt;
+                        fi
+						#remove lines that already checked
+						sed -i 1,"$X"d $FILE
+                        echo -e -n "Number of files: $c_check"
+                        echo -e "+++++++++++++++++++++++++++"
+				 done
+			rm -rf $FILE
     fi
 done
